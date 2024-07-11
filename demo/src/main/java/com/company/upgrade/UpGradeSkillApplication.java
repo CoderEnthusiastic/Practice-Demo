6cf3f21df899c29ee87e5fdcdd0cd9baaf33428c
CoderEnthusiastic <rajatkinlekar99@gmail.com>
2024-07-12 01:04:28 +0530

    first commit

---
+++ demo/src/main/java/com/company/upgrade/UpGradeSkillApplication.java
@@ -0,0 +1,13 @@
+package com.company.upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpGradeSkillApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ demo/src/test/java/com/company/upgrade/UpGradeSkillApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpGradeSkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+		GameRunner runner = new GameRunner();
+	}
+
+}
